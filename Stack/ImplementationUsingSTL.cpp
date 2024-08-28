f2237bf3f2cc2685b8433db7f7c16adf5a78d11c
Anony-Shubh <21052106@kiit.ac.in>
2024-08-28 23:03:11 +0530

    STL implementation

---
+++ Stack/ImplementationUsingSTL.cpp
@@ -0,0 +1,24 @@
+#include<stack>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    stack<string> st;
+    int n;
+    cout<<"Enter size of stack: ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        string val;
+        cin>>val;
+        st.push(val);
+    }
+    while(!st.empty())
+    {
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    return 0;
+}
